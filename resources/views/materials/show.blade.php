5f99493921836d6f1132090d4b073dbd8b4190aa
prayogisatya-ofc <besutodio@gmail.com>
2025-06-14 12:54:59 +0700

    update: show materi lebih better

--- resources/views/materials/show.blade.php
+++ resources/views/materials/show.blade.php
@@ -4,10 +4,6 @@
     {{ $material->title }}
 @endsection
 
-@push('style')
-<link rel="stylesheet" href="https://pdfjs.express/assets/css/pdf-viewer.css" />
-@endpush
-
 @section('content')
     <div class="container-fluid">
         <div class="card bg-primary-subtle shadow-none position-relative overflow-hidden mb-4">
@@ -37,66 +33,49 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-body">
                         @if ($material->type === 'pdf')
-                            <div id="pdf-viewer" class="border" style="height: 600px;"></div>
-                            {{-- <iframe
-                                src="https://mozilla.github.io/pdf.js/web/viewer.html?file={{ asset('storage/' . $material->source) }}"
-                                width="100%" height="100%" style="border: none;">
-                            </iframe> --}}
+                            <a href="{{ asset('storage/' . $material->source) }}" target="_blank">
+                                <img src="{{ asset('assets/images/thumbnail-show-pdf.jpg') }}" alt="Show PDF" class="w-100 rounded">
+                            </a>
                         @endif
+
+                        @if ($material->content)
+                            <div class="mt-5">
+                                <h5 class="fw-semibold mb-3">Isi Materi</h5>
+                                {!! $material->content !!}
+                            </div>
+                        @endif
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="fw-semibold mb-3">Informasi Materi</h5>
+                        <div class="table-responsive">
+                            <table class="w-100">
+                                <tbody>
+                                    <tr>
+                                        <th class="pb-1">Penulis</th>
+                                        <td class="pb-1 text-end">{{ $material->user->name }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="py-1">Tipe Materi</th>
+                                        <td class="py-1 text-end text-uppercase">{{ $material->type }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="pt-1">Tanggal Pembuatan</th>
+                                        <td class="pt-1 text-end">{{ $material->created_at->format('d-m-Y') }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 @endsection
-
-@push('scripts')
-    <script src="//mozilla.github.io/pdf.js/build/pdf.mjs" type="module"></script>
-    <script type="module">
-        var url = "{{ asset('storage/' . $material->source) }}";
-
-        // Loaded via <script> tag, create shortcut to access PDF.js exports.
-        var { pdfjsLib } = globalThis;
-
-        // The workerSrc property shall be specified.
-        pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.mjs';
-
-        // Asynchronous download of PDF
-        var loadingTask = pdfjsLib.getDocument(url);
-        loadingTask.promise.then(function(pdf) {
-            console.log('PDF loaded');
-
-            // Fetch the first page
-            var pageNumber = 1;
-            pdf.getPage(pageNumber).then(function(page) {
-            console.log('Page loaded');
-
-            var scale = 1.5;
-            var viewport = page.getViewport({scale: scale});
-
-            // Prepare canvas using PDF page dimensions
-            var canvas = document.getElementById('pdf-viewer');
-            var context = canvas.getContext('2d');
-            canvas.height = viewport.height;
-            canvas.width = viewport.width;
-
-            // Render PDF page into canvas context
-            var renderContext = {
-                canvasContext: context,
-                viewport: viewport
-            };
-            var renderTask = page.render(renderContext);
-            renderTask.promise.then(function () {
-                console.log('Page rendered');
-            });
-            });
-        }, function (reason) {
-            // PDF loading error
-            console.error(reason);
-        });
-    </script>
-@endpush
